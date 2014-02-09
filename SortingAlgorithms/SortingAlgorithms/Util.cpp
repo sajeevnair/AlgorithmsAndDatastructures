e2c1d5472289dbe2dc8d84671b94296b58b14aa8
Sajeev Nair <Sajeev.nair174@gmail.com>
2014-02-08 23:12:22 -0800

    Basic Setup
    attempt to modularize the application.
    created a function that prints an array

---
+++ SortingAlgorithms/SortingAlgorithms/Util.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+
+void printArray(int * a, int sizeOfArray){
+	for (int i = 0; i < sizeOfArray; i++)
+	{
+		cout << a[i] << "\t";
+
+	}
+	cout << endl;
+
+
+}
---
+++ SortingAlgorithms/SortingAlgorithms/main.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+
+int main(){
+
+	int arr[] = { 4, 2, 5, 9, 1 };
+	printArray(arr, sizeof(arr)/sizeof(int));
+	getchar();
+	return 0;
+
+}
